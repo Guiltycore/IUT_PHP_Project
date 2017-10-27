85b22d64a7f3815a452f974a4b414a6dd1dad23b
Yves DANIEL <yves.daniel11@gmail.com>
2017-10-27 12:15:48 +0200

    Edit delete.php

--- view/produit/delete.php
+++ view/produit/delete.php
@@ -5,6 +5,6 @@
 	 * Date: 27/10/17
 	 * Time: 11:45
 	 */
-	echo '<p>La voiture a bien été supprimé !</p>';
+	echo '<p>Le produit a bien été supprimé !</p>';
 	require File ::build_path ( [ 'view' , 'produit' , 'list.php' ] );
 ?>
