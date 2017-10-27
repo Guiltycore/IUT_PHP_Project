f1883b803a1bc1d8321f52d227732d197b8ee01f
Yves DANIEL <yves.daniel11@gmail.com>
2017-10-27 12:14:51 +0200

    Edit List

--- view/produit/list.php
+++ view/produit/list.php
@@ -1,6 +1,11 @@
+
 <?php
 	// Display of the products stored in $tab_p
+	echo "<br>Liste des produits<br>";
 	foreach ( $tab_p as $p ) {
 		echo '<p> <a href=\'./index.php?controller=produit&action=read&'.ModelProduit::getPrimary () .'=' . rawurlencode ( $p -> getID_p () ) . '\'>' . htmlspecialchars ( $p -> getNom_p () ) . '</a>.</p>';
+
 	}
 ?>
+<a href="index.php?controller=produit&action=update">Créer produit</a>
+
