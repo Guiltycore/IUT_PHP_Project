7dfdd652fbae52c71824aabe00c81a465d5fc798
Yves DANIEL <yves.daniel11@gmail.com>
2017-10-27 12:05:24 +0200

    Edit detail

--- view/produit/detail.php
+++ view/produit/detail.php
@@ -9,7 +9,7 @@
 		echo "<p>
 Nom:" . $p -> getNom_p () . "
 <br>Prix: " . $p -> getPrix_p () . "
-<br>Description<br><br>:" . $p -> getDescription_p ();
+<br>Description:<br><br>" . $p -> getDescription_p ();
 		echo "<br><br><a href=index.php?controller=produit&action=update&" . ModelProduit ::getPrimary () . "=" . rawurlencode ( $p -> getID_p () ) . ">Update</a> <a href=index.php?controller=produit&action=delete&" . ModelProduit ::getPrimary () . "=" . rawurlencode ( $p -> getID_p () ) . ">Delete</a> </p>";
 	} else {
 		require File ::build_path ( [ 'view' , 'utilisateur' , 'error.php' ] );
