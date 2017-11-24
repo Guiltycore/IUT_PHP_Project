6689c79629440d9e187bbf51efc0d9b57c6a017f
Yves DANIEL <yves.daniel11@gmail.com>
2017-11-24 11:44:05 +0100

    Modification de index.php

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
 	 * Time: 10:07
 	 */
 	include __DIR__.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'File.php';
-
+	include File::build_path (['lib','Security.php']);
+	include File::build_path (['lib','Session.php']);
 	require File::build_path (array ('controller','routeur.php'));
 
 ?>
