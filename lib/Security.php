aca96fad453173544b757bd0cacae57bd6368bcd
Yves DANIEL <yves.daniel11@gmail.com>
2017-11-24 11:54:41 +0100

    Update Security.php

--- lib/Security.php
+++ lib/Security.php
@@ -20,6 +20,13 @@
 			$texte_chiffre = hash('sha256', $texte_en_clair);
 			return $texte_chiffre;
 		}
+		static function generateRandomHex() {
+			// Generate a 32 digits hexadecimal number
+			$numbytes = 16; // Because 32 digits hexadecimal = 16 bytes
+			$bytes = openssl_random_pseudo_bytes($numbytes);
+			$hex   = bin2hex($bytes);
+			return $hex;
+		}
 	}
 
 ?>
