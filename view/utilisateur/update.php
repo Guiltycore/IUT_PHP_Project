74511509bafa4c24172b4cea56aa4e1dafd7e080
Hutama <32954054+Renaud-Fournier@users.noreply.github.com>
2017-11-24 12:07:40 +0100

    quotes corrections

--- view/utilisateur/update.php
+++ view/utilisateur/update.php
@@ -12,15 +12,15 @@
 		
 		<label for=\"mdp_id\">Mot de passe</label> :
 		<input type=\"password\" placeholder=\"Ex : MyLifeForAïur\" name=\"mdp\" id=\"mdp_id\" required/>	
-		<label for='mdp_conf_id'>Confirmer mot de passe</label>
-		<input type='password' placeholder='Ex : MyLifeForAïur\' name='mdp_conf' id='mdp_conf_id' required/>
+		<label for=\"mdp_conf_id\">Confirmer mot de passe</label>
+		<input type=\"password\" placeholder=\"Ex : MyLifeForAïur\" name=\"mdp_conf\" id=\"mdp_conf_id\" required/>
 		
 		<label for=\"adr_id\">Adresse</label> :
 		<input type=\"adresse\" placeholder=\"Ex : 3 rue Shakura\" name=\"adresse\" id=\"adr_id\" value=\"" . $v -> getAdresse () . "\" required/>		
 		
-		<input type='hidden' name='login' value='".$v->getLogin()."'>
-		<input type='hidden' name='action' value='updated'>
-		<input type='hidden' name='controller' value='utilisateur'>
+		<input type=\"hidden\" name=\"login\" value=\"".$v->getLogin()."\">
+		<input type=\"hidden\" name=\"action\" value=\"updated\">
+		<input type=\"hidden\" name=\"controller\" value=\"utilisateur\">
 	</p>
 	<p>
 		<input type=\"submit\" value=\"Envoyer\"/>
@@ -41,8 +41,8 @@
 		
 		<label for=\"mdp_id\">Mot de passe</label> :
 		<input type=\"password\" placeholder=\"Ex : EnTaroTassadar\" name=\"mdp\" id=\"mdp_id\" required/>
-		<label for='mdp_conf_id'>Confirmer mot de passe</label>
-		<input type='password' placeholder='Ex : EnTaroTassadar\' name='mdp_conf' id='mdp_conf_id' required/>
+		<label for=\"mdp_conf_id\">Confirmer mot de passe</label>
+		<input type=\"password\" placeholder=\"Ex : EnTaroTassadar\" name=\"mdp_conf\" id=\"mdp_conf_id\" required/>
 		
 		<label for=\"adr_id\">Adresse</label> :
 		<input type=\"adresse\" placeholder=\"Ex :3 rue Adun\" name=\"adresse\" id=\"adr_id\" required/>
@@ -50,8 +50,8 @@
 		<label for=\"mail_id\">Mail</label>
 		<input name=\"mail\" id=\"mail_id\" type=\"email\">
 		
-		<input type='hidden' name='action' value='created'>
-		<input type='hidden' name='controller' value='utilisateur'>
+		<input type=\"hidden\" name=\"action\" value=\"created\">
+		<input type=\"hidden\" name=\"controller\" value=\"utilisateur\">
 	</p>
 	<p>
 		<input type=\"submit\" value=\"Envoyer\"/>
