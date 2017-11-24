37915208b60ccf1a7399427871f0ab63ccbff40e
Hutama <32954054+Renaud-Fournier@users.noreply.github.com>
2017-11-24 10:32:32 +0100

    Update view.php

--- view/view.php
+++ view/view.php
@@ -3,8 +3,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<title><?php echo $pagetitle; ?></title>
-		<link rel="stylesheet" type="text/css" href="css/style.css"
-
+		<link rel="stylesheet" type="text/css" href="css/style.css">
 	</head>
 	<body>
 		<header>
