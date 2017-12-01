7006fd04427b20c40afdba034b039663dc32ce52
Hutama <32954054+Renaud-Fournier@users.noreply.github.com>
2017-12-01 10:15:35 +0100

    Update ControllerProduit.php

--- controller/ControllerProduit.php
+++ controller/ControllerProduit.php
@@ -86,4 +86,12 @@
 			}
 			return $randomString;
 		}
+		private static function ajouterAuPanier($idp){
+			if (!isset($_COOKIE['panier'])) {	//creation du panier (si inexistant)
+				setcookie("panier", "idp", time()+60); 
+			}
+			self::readAll (1);
+
+
+		}
 	}
