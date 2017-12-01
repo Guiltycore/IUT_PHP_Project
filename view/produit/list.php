7de319b7b67bf2fd1a8d63c2afa9638df91a7648
Yves DANIEL <yves.daniel11@gmail.com>
2017-12-01 10:22:36 +0100

    Connect list

--- view/produit/list.php
+++ view/produit/list.php
@@ -3,7 +3,25 @@
 	// Display of the products stored in $tab_p
 	echo "<br>Liste des produits<br>";
 	foreach ( $tab_p as $p ) {
-		echo '<p><img src=\''.$p->getPicP().'\' alt=\'Product Picture\' height="80" width="80"><br><a href=\'./index.php?controller=produit&action=read&'.ModelProduit::getPrimary () .'=' . rawurlencode ( $p -> getID_p () ) . '\'>' . htmlspecialchars ( $p -> getNom_p () ) . '</a>.</p>';
+		echo '<p><img src=\''
+			.$p->getPicP()
+			.'\' alt=\'Product Picture\' height="80" width="80">
+			<br>
+			<a href=\'./index.php?controller=produit&action=read&'
+			.ModelProduit::getPrimary ()
+			.'='
+			. rawurlencode ( $p -> getID_p () )
+			. '\'>'
+			. htmlspecialchars ( $p -> getNom_p () )
+			. '</a>.';
+		echo "<form method=\"get\" action=\"index.php?action=ap&controller=produit\">
+				<input type='hidden' name='id_p' value='".$p->getID_p()."'>
+				<input type=\"submit\" value=\"Ajouter au panier\"/>
+
+		</form>
+		";
+
+		echo '</p>';
 	}
 	echo "<div>";
 	if($page>1){
