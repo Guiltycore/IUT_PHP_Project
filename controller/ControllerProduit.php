b02932d328004ec389447a9f2b95d91befacca51
Yves DANIEL <yves.daniel11@gmail.com>
2017-12-03 02:16:47 +0100

    Update ControllerProduit.php

--- controller/ControllerProduit.php
+++ controller/ControllerProduit.php
@@ -91,14 +91,13 @@
 
 			if (!isset($_COOKIE['panier'])) {
 				$tab = ["".$idp=>1];
-				setcookie("popo","ab",time()+3600);
+				print_r($tab);
 				setcookie("panier", serialize($tab), time()+3600);
 			}
 			else{
 				$tab = unserialize($_COOKIE["panier"]);
 				$var = isset($tab["".$idp])?$tab["".$idp]:0;
 				$tab["".$idp]=$var + 1;
-				setcookie("popo","ab",time()+3600);
 				setcookie("panier", serialize($tab), time()+3600);
 			}
 			self::readAll (1);
